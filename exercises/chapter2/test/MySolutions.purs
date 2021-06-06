0295a5c74105797b9eb41620e1143f6d515fd84e
Paul <paul.constantin.chitic@gmail.com>
2021-06-06 06:15:52 +0300

    chapter2

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,11 @@
 module Test.MySolutions where
 
 import Prelude
+import Math (sqrt, pi)
+import Data.Int (rem)
+
+diagonal w h = sqrt (w * w + h * h)
+
+circleArea r = pi * (r * r)
+
+leftoverCents m = rem m 100
